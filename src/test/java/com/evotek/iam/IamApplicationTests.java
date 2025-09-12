310dda90ddb790fc75e30f01865ce6683c23b815
minh220601 <tranminh2262001@gmail.com>
2025-09-12 11:34:21 +0700

    first commit

---
+++ src/test/java/com/evotek/iam/IamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evotek.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
