13451eab88f51b2bc86aeb29034041c8110337a0
minh220601 <tranminh2262001@gmail.com>
2025-09-12 15:47:46 +0700

    change

--- src/main/java/com/evotek/iam/model/Role.java
+++ src/main/java/com/evotek/iam/model/Role.java
@@ -1,4 +1,41 @@
 package com.evotek.iam.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "name")
+    String name;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "role_id")
+    Collection<User> users = new HashSet<>();
+
+    public void assignRoleToUser(User user){
+        user.setRole(this);
+        if(users == null){
+            users = new HashSet<>();
+        }
+        this.getUsers().add(user);
+    }
+
+    public void removeUserFromRole(User user){
+        this.getUsers().remove(user);
+    }
 }
--- src/main/java/com/evotek/iam/model/User.java
+++ src/main/java/com/evotek/iam/model/User.java
@@ -1,4 +1,45 @@
 package com.evotek.iam.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "full_name")
+    String fullName;
+
+    @Column(name = "birth_date")
+    LocalDate birthDate;
+
+    @Column(name = "phone")
+    String phone;
+
+    @Column(name = "address")
+    String address;
+
+    @Column(name = "email")
+    String email;
+
+    @Column(name = "password")
+    String password;
+
+    @Column(name = "avatar")
+    String avatar;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH})
+    Role role;
 }
