d217024da0bbe8cf09e7b893210e1ee4e7b5bfc6
minh220601 <tranminh2262001@gmail.com>
2025-09-16 08:25:22 +0700

    init commit

--- src/main/java/com/evotek/iam/repository/RoleRepository.java
+++ src/main/java/com/evotek/iam/repository/RoleRepository.java
@@ -3,5 +3,5 @@ package com.evotek.iam.repository;
 import com.evotek.iam.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Integer> {
+public interface RoleRepository extends JpaRepository<Role> {
 }
